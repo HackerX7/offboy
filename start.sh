ced2cf5b1cebab79dc4d204a99e1e247624a5f97
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-08-14 13:16:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Hacker-Jr-TG/NEW-bot.git /NEW-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /NEW-bot
 fi
-cd /EvaMaria
+cd /NEW-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
