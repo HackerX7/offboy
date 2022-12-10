6cb1e9a06d0d662eac59a9116c90191d33ef0b3e
SpiDer-HacKer Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-12-10 14:51:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -795,7 +795,7 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
+    await msg.reply_photo(photo="https://telegra.ph/file/9965d1aa79593e56f212a.jpg", caption="I couldn't find anything related to that\nDid you mean any one of these?",
                     reply_markup=InlineKeyboardMarkup(btn))
 
 
