30abdb59e199f7b9777d7bced97e85cbc6fc5c11
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-12-21 12:49:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,9 +19,7 @@ logger.setLevel(logging.INFO)
 
 BTN_URL_REGEX = re.compile(
     r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))"
-)
-
-imdb = Cinemagoer() 
+) 
 
 BANNED = {}
 SMART_OPEN = '“'
